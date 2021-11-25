7f9f635d023f6761104a559f2d0e09579f481115
jose <jl.rodriguez@iesdoctorbalmis.com>
2021-11-25 14:17:31 +0100

    Modificación

---
+++ Comida/MainWindowsVM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comida
+{
+    class MainWindowsVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> comidas;
+        public ObservableCollection<string> Comidas
+        {
+
+
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
--- Comida/Plato.cs
+++ Comida/Plato.cs
@@ -1,40 +1,45 @@
-﻿class Plato
+﻿using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Comida
 {
-    public string Nombre { get; set; }
-    public string Imagen { get; set; }
-    public string Tipo { get; set; }
-    public bool Gluten { get; set; }
-    public bool Soja { get; set; }
-    public bool Leche { get; set; }
-    public bool Sulfitos { get; set; }
-
-    public Plato(string nombre, string imagen, string tipo, bool gluten, bool soja, bool leche, bool sulfitos)
+    class Plato
     {
-        Nombre = nombre;
-        Imagen = imagen;
-        Tipo = tipo;
-        Gluten = gluten;
-        Soja = soja;
-        Leche = leche;
-        Sulfitos = sulfitos;
-    }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        public string Tipo { get; set; }
+        public bool Gluten { get; set; }
+        public bool Soja { get; set; }
+        public bool Leche { get; set; }
+        public bool Sulfitos { get; set; }
 
-    public Plato()
-    {
-    }
+        public Plato(string nombre, string imagen, string tipo, bool gluten, bool soja, bool leche, bool sulfitos)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Tipo = tipo;
+            Gluten = gluten;
+            Soja = soja;
+            Leche = leche;
+            Sulfitos = sulfitos;
+        }
 
-    public static ObservableCollection<Plato> GetSamples(string rutaImagenes)
-    {
-        ObservableCollection<Plato> lista = new ObservableCollection<Plato>();
+        public Plato()
+        {
+        }
 
-        lista.Add(new Plato("Hamburguesa", Path.Combine(rutaImagenes, @"burguer.jpg"), "Americana", true, false, true, true));
-        lista.Add(new Plato("Dumplings", Path.Combine(rutaImagenes, @"dumplings.jpg"), "China", true, true, false, false));
-        lista.Add(new Plato("Tacos", Path.Combine(rutaImagenes, @"tacos.jpg"), "Mexicana", true, false, false, true));
-        lista.Add(new Plato("Cerdo agridulce", Path.Combine(rutaImagenes, @"cerdoagridulce.jpg"), "China", true, true, false, true));
-        lista.Add(new Plato("Hot dogs", Path.Combine(rutaImagenes, @"hotdog.jpg"), "Americana", true, true, true, true));
-        lista.Add(new Plato("Fajitas", Path.Combine(rutaImagenes, @"fajitas.jpg"), "Mexicana", true, false, false, true));
+        public static ObservableCollection<Plato> GetSamples(string rutaImagenes)
+        {
+            ObservableCollection<Plato> lista = new ObservableCollection<Plato>();
 
-        return lista;
+            lista.Add(new Plato("Hamburguesa", Path.Combine(rutaImagenes, @"burguer.jpg"), "Americana", true, false, true, true));
+            lista.Add(new Plato("Dumplings", Path.Combine(rutaImagenes, @"dumplings.jpg"), "China", true, true, false, false));
+            lista.Add(new Plato("Tacos", Path.Combine(rutaImagenes, @"tacos.jpg"), "Mexicana", true, false, false, true));
+            lista.Add(new Plato("Cerdo agridulce", Path.Combine(rutaImagenes, @"cerdoagridulce.jpg"), "China", true, true, false, true));
+            lista.Add(new Plato("Hot dogs", Path.Combine(rutaImagenes, @"hotdog.jpg"), "Americana", true, true, true, true));
+            lista.Add(new Plato("Fajitas", Path.Combine(rutaImagenes, @"fajitas.jpg"), "Mexicana", true, false, false, true));
+
+            return lista;
+        }
     }
 }
-
